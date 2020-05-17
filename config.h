bab058531e264b78302384ff858452a31e6b355b
Samuel Dezso Dysch <sdysch94@gmail.com>
2020-05-17 23:44:51 +0100

    Fonts

--- config.h
+++ config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font    = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font2[] = { "Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
@@ -470,6 +471,7 @@ static const char *colorname[] = {
  */
 ResourcePref resources[] = {
 		{ "font",         STRING,  &font },
+		{ "fontalt0",     STRING,  &font2[0] },
 		{ "color0",       STRING,  &colorname[0] },
 		{ "color1",       STRING,  &colorname[1] },
 		{ "color2",       STRING,  &colorname[2] },
